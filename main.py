ef2004094ed54a160de3e29efc05c98e00dbd6e0
jorgeroor <64564377+jorgeroor@users.noreply.github.com>
2024-11-20 19:52:31 -0600

    Add files via upload
    Added HTTP authentication

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+import secrets
+from typing import Annotated
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from model.user_connection import UserConnection
 from starlette.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
 from schema.user_schema import UserSchema
@@ -6,6 +9,26 @@ from schema.user_schema import UserSchema
 
 app = FastAPI()
 conn = UserConnection()
+security = HTTPBasic()
+correct_username_bytes = b"admin"
+correct_password_bytes = b"okn947V*"
+
+def get_current_username(credentials: Annotated[HTTPBasicCredentials, Depends(security)],):
+    current_username_bytes = credentials.username.encode("utf8")
+    is_correct_username = secrets.compare_digest(
+        current_username_bytes, correct_username_bytes
+    )
+    current_password_bytes = credentials.password.encode("utf8")
+    is_correct_password = secrets.compare_digest(
+        current_password_bytes, correct_password_bytes
+    )
+    if not (is_correct_username and is_correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return True
 
 
 @app.get("/api", status_code=HTTP_200_OK)
@@ -36,7 +59,7 @@ def get_one(id:str):
 
 
 @app.post("/api/insert", status_code=HTTP_201_CREATED)
-def insert(user_data:UserSchema):
+def insert(user_data:UserSchema, authenticated: Annotated[str, Depends(get_current_username)]):
     data = user_data.model_dump()
     data.pop("id")
     print(data)
@@ -44,12 +67,12 @@ def insert(user_data:UserSchema):
 
 
 @app.put("/api/update/{id}", status_code=HTTP_204_NO_CONTENT)
-def update(user_data:UserSchema, id:str):
+def update(user_data:UserSchema, id:str, authenticated: Annotated[str, Depends(get_current_username)]):
     data = user_data.model_dump()
     data["id"] = id
     conn.update(data)
  
 
 @app.delete("/api/delete/{id}", status_code=HTTP_204_NO_CONTENT)
-def delete(id:str):
+def delete(id:str, authenticated: Annotated[str, Depends(get_current_username)]):
     conn.delete(id)
